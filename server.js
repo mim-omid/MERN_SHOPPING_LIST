8cf9fb5af56125f8630aff9a7900ea3a2bf87bcd
mim-omid <mr.mim.omid@gmail.com>
2018-11-17 20:46:23 +0330

    added routes folder and route get for all items

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+const items = require('./routes/api/items');
+
+
 const app = express();
 
 // Bodyparser Middleware
@@ -14,6 +17,9 @@ const db = require('./config/keys').mongoURI;
 mongoose.connect(db)
     .then(()=> console.log('MongoDB Connected ...'))
     .catch(err => console.log(err));
+
+// Use Routes
+app.use('/api/items', items); 
 
 const port = process.env.PORT || 5000;
 
