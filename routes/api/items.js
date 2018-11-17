680e09495941383757db6887900d14c0534287c6
mim-omid <mr.mim.omid@gmail.com>
2018-11-17 21:06:08 +0330

    added route for  delete item

--- routes/api/items.js
+++ routes/api/items.js
@@ -14,7 +14,7 @@ router.get('/',(req, res) => {
 });
 
 // @route POST api/items
-//@desc Create A Post
+//@desc Create An Item
 // @access Public
 router.post('/',(req, res) => {
     const newItem = new Item({
@@ -24,6 +24,16 @@ router.post('/',(req, res) => {
     newItem.save()
         .then(item => res.json(item));
 });
+
+// @route DELETE api/items/:id
+//@desc Delete An Item
+// @access Public
+router.delete('/:id',(req, res) => {
+    Item.findById(req.params.id)
+        .then(item => item.remove().then(() => res.json({success: true})))
+        .catch(err => res.status(404).json({success: false}));
+});
+
 
 
 module.exports = router;
